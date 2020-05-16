c9d1df52c85a74d2c9f8ad8b2dac1652bc052949
JithinJude <jithin.jude68@gmail.com>
2020-05-16 06:48:32 +0530

    feedback format change.

--- app/src/main/java/com/ajce/hostelmate/Utils.kt
+++ app/src/main/java/com/ajce/hostelmate/Utils.kt
@@ -18,18 +18,18 @@ class Utils : Application() {
 
         fun getMonthInThreeCapLetter(month: Int): String{
             when (month){
-                1 -> { return "JAN"}
-                2 -> { return "FEB"}
-                3 -> { return "MAR"}
-                4 -> { return "APR"}
-                5 -> { return "MAY"}
-                6 -> { return "JUN"}
-                7 -> { return "JUL"}
-                8 -> { return "AUG"}
-                9 -> { return "SEP"}
-                10 -> { return "OCT"}
-                11 -> { return "NOV"}
-                12 -> { return "DEC"}
+                1 -> { return "01JAN"}
+                2 -> { return "02FEB"}
+                3 -> { return "03MAR"}
+                4 -> { return "04APR"}
+                5 -> { return "05MAY"}
+                6 -> { return "06JUN"}
+                7 -> { return "07JUL"}
+                8 -> { return "08AUG"}
+                9 -> { return "09SEP"}
+                10 -> { return "10OCT"}
+                11 -> { return "11NOV"}
+                12 -> { return "12DEC"}
                 else -> {
                     return "ERROR"
                 }
