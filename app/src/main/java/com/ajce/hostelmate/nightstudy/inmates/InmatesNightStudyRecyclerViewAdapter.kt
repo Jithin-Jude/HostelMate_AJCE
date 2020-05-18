c9bb840ca5cabebec3c84359a9e99a833aa814e0
jonahms <jonahmariasebastian2020@cs.ajce.in>
2020-05-18 23:49:54 +0530

    night study completed

--- app/src/main/java/com/ajce/hostelmate/nightstudy/inmates/InmatesNightStudyRecyclerViewAdapter.kt
+++ app/src/main/java/com/ajce/hostelmate/nightstudy/inmates/InmatesNightStudyRecyclerViewAdapter.kt
@@ -18,23 +18,23 @@ import com.ajce.hostelmate.sickleave.SickLeave
 class InmatesNightStudyRecyclerViewAdapter(var context: Context?,
                                            nightStudyList: MutableList<NightStudy?>?) :
         RecyclerView.Adapter<InmatesNightStudyRecyclerViewAdapter
-        .InmatesSickLeaveRecyclerViewHolder?>() {
+        .InmatesNightStudyRecyclerViewHolder?>() {
     private val nightStudyList: MutableList<NightStudy?>?
     private val mInflater: LayoutInflater?
 
-    val SELECTED_SICK_LEAVE: String = "selected_sick_leave"
+    val SELECTED_NIGHT_STUDY: String = "selected_night_study"
     val SELECTED_POSITION: String = "selected_position"
 
     // inflates the row layout from xml when needed
     override fun onCreateViewHolder(parent: ViewGroup, pos: Int):
-            InmatesNightStudyRecyclerViewAdapter.InmatesSickLeaveRecyclerViewHolder {
+            InmatesNightStudyRecyclerViewAdapter.InmatesNightStudyRecyclerViewHolder {
         val view = mInflater?.inflate(R.layout.recyclerview_item_sickleave, parent, false)
-        return InmatesSickLeaveRecyclerViewHolder(view)
+        return InmatesNightStudyRecyclerViewHolder(view)
     }
 
     // binds the data to the TextView in each row
     override fun onBindViewHolder(holderInmatesSickLeave: InmatesNightStudyRecyclerViewAdapter
-    .InmatesSickLeaveRecyclerViewHolder, pos: Int) {
+    .InmatesNightStudyRecyclerViewHolder, pos: Int) {
         holderInmatesSickLeave.title?.text = nightStudyList?.get(pos)?.nightStudyTitle
         val issueLocation = nightStudyList?.get(pos)?.nightStudyBlock + ", " + nightStudyList?.get(pos)?.nightStudyRoom
         holderInmatesSickLeave.blockAndRoom?.text = issueLocation
@@ -56,10 +56,10 @@ class InmatesNightStudyRecyclerViewAdapter(var context: Context?,
                 .into(holderInmatesSickLeave.imageView!!)*/
 
         holderInmatesSickLeave.item?.setOnClickListener {
-            /*val intent = Intent(context, InmatesSickLeavesDetailsActivity::class.java)
-            intent.putExtra(SELECTED_SICK_LEAVE, nightStudyList?.get(pos))
+            val intent = Intent(context, InmatesNightStudyDetailsActivity::class.java)
+            intent.putExtra(SELECTED_NIGHT_STUDY, nightStudyList?.get(pos))
             intent.putExtra(SELECTED_POSITION,pos)
-            context!!.startActivity(intent)*/
+            context!!.startActivity(intent)
 
         }
     }
@@ -76,7 +76,7 @@ class InmatesNightStudyRecyclerViewAdapter(var context: Context?,
         this.nightStudyList = nightStudyList
     }
 
-    inner class InmatesSickLeaveRecyclerViewHolder(itemView: View?) :
+    inner class InmatesNightStudyRecyclerViewHolder(itemView: View?) :
             RecyclerView.ViewHolder(itemView!!) {
         var item: CardView?
         var title: TextView?
